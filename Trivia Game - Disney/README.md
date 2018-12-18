cd1ad4d616ff76c7cd036692a0ee51dfa8dec2ec
Whitman Meeks Wirthlin <44418422+Thesearenotthejedisyourelookingfor@users.noreply.github.com>
2018-12-18 14:58:47 -0500

    Add files via upload

---
+++ Trivia Game - Disney/README.md
@@ -0,0 +1,2 @@
+#Disney Trivia
+This is a Disney Trivia Game consisting of five Disney related questions. These questions are designed to test the limits of your imaginations with Disney....Be Prepared!!
---
+++ Trivia Game - Disney/assets/javascript/app.js
@@ -0,0 +1,199 @@
+$(document).ready(function() {
+	var index = 0;
+	var countdownTimer = {
+		time : 30,
+		reset: function() {
+			this.time = 30;
+			$('.timer').html('<h3>' + this.time + ' seconds remaining</h3>');
+		},
+		start: function() {
+			counter = setInterval(countdownTimer.count, 1000);	
+		},
+		stop: function() {
+			clearInterval(counter);
+		},
+		count: function() {
+				countdownTimer.time--;
+				console.log(countdownTimer.time);
+//				$('.timer').html(countdownTimer.time);
+			if (countdownTimer.time >= 0) {
+				$('.timer').html('<h3>' + countdownTimer.time + ' seconds remaining</h3>');
+			}
+			else {
+				index++;
+				answerWrong();
+				countdownTimer.reset();
+				if (index < questionArray.length) {
+					loadQuestion(index);
+				} else {
+					$(".answerchoice").hide();
+					showScore();
+				}
+			}
+		}
+	};
+
+var correct = 0;
+var wrong = 0;
+var q1 = {
+	question : 'According to Disney - What is the Beasts origin?',
+	possibleAnswers : ['A. Elephant',
+				 'B. Dog',
+				 'C. Cat',
+				 'D. A little bit of everything'],
+	flags : [false, false, false, true],
+	answer : 'D. A little bit of everything'
+};
+
+var q2 = {
+	question: '2 Yen Sid is Disney Backwards in What popular film?',
+	possibleAnswers: ['A. Harry Potter',
+				 'B. The Matrix',
+				 'C. The Sorcerers Apprentice',
+				 'D. Lord of the Rings'],
+	flags : [false, false, true, false],
+	answer : 'C. The Sorcerers Apprentice'
+};
+
+var q3 = {
+	question : 'What is the funny disclaimer at the end of Frozen referring too?',
+	possibleAnswers : ['A. Elsa',
+				 'B. Anna',
+				 'C. Olaf',
+				 'D. Kristoff'],
+	flags : [false, false, false, true],
+	answer : 'D. Kristoff'
+};
+
+var q4 = {
+	question : 'Robin William Plays What Character in Aladdin?',
+	possibleAnswers : ['A. Genie',
+				 'B. Aladdin',
+				 'C. The Peddler',
+				 'D. A & C'],
+	flags : [false, false, false, true],
+	answer : 'D. A & C',
+};
+
+var q5 = {
+	question : 'The Robot WALL-E Got his Name after Who?',
+	possibleAnswers : ['A. Michael Jordan',
+				 'B. Walter Elias Disney',
+				 'C. It is a made up name',
+				 'D. Whitman'],
+	flags : [false, true, false, false],
+	answer : 'B. Walter Elias Disney'
+};
+
+var questionArray = [q1, q2, q3, q4, q5];
+
+function loadQuestion(questionSelection) {
+	console.log(questionSelection);
+	countdownTimer.reset();
+  $(".question").html("<h3>" + questionArray[questionSelection].question + "</h3>");
+  $("#buttonA").text(questionArray[questionSelection].possibleAnswers[0]).show();
+  $("#buttonB").text(questionArray[questionSelection].possibleAnswers[1]).show();
+  $("#buttonC").text(questionArray[questionSelection].possibleAnswers[2]).show();
+  $("#buttonD").text(questionArray[questionSelection].possibleAnswers[3]).show();
+
+}
+
+
+
+function setup() {
+	index = 0;
+	$('.question').append('<button id="startButton">Start</button>');
+	$('#startButton').on('click', function() {
+		$(this).hide();
+		countdownTimer.start();
+	 	loadQuestion(index);
+	});
+}		
+
+function getAnswer() {
+
+
+	$('.answerchoice').on('click', function() {
+	  console.log('alert', index);
+		index++;
+		console.log('click', index);
+		$(".question").text('');
+		$("#buttonA").text('');
+		$("#buttonB").text('');
+		$("#buttonC").text('');
+		$("#buttonD").text('');
+		loadQuestion();
+	})
+}
+
+function answerCorrect() {
+	correct++;
+	alert("Correct!");
+	console.log("correct");
+}
+
+function answerWrong() {
+	wrong++;
+	alert("Incorrect!");
+	console.log("wrong");
+}
+
+function showScore() {
+	$('.question').empty();
+	$('.question').append("<h2><p>" + correct + " correct</p></h2>");
+	$('.question').append("<h2><p>" + wrong + " incorrect</p></h2>");
+	countdownTimer.stop();
+	$('.timer').empty();
+
+}
+
+setup();
+$('.answerchoice').on('click', function() {
+ console.log($(this));
+ if(this.id == 'buttonA') {
+ 	var answerChosen = 'A';
+ } else if(this.id == 'buttonB') {
+ 	answerChosen = 'B';
+ } else if (this.id == 'buttonC') {
+ 	answerChosen = 'C';
+ } else if (this.id == 'buttonD') {
+ 	answerChosen = 'D';
+ } 
+ if ((answerChosen == 'A') && (questionArray[index].flags[0] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'A') {
+ 	answerWrong();
+ }
+ if ((answerChosen == 'B') && (questionArray[index].flags[1] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'B') {
+ 	answerWrong();
+ }
+if ((answerChosen == 'C') && (questionArray[index].flags[2] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'C') {
+ 	answerWrong();
+ }
+if ((answerChosen == 'D') && (questionArray[index].flags[3] == true)) {
+ 	answerCorrect();
+ } else if (answerChosen == 'D') {
+ 	answerWrong();
+ }
+
+ $(".question").text('');
+ $("#buttonA").text('');
+ $("#buttonB").text('');
+ $("#buttonC").text('');
+ $("#buttonD").text('');
+ index++;
+ if (index < questionArray.length) {
+ 	loadQuestion(index);
+ } else {
+ 	$(".answerchoice").hide();
+ 	showScore();
+ }
+});
+
+
+//	$('#start').click(countdownTimer.start);
+});
